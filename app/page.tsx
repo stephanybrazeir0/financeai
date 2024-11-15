2345d355c166f5a92e52547f4b652484cfebfc86
Stephany Brazeiro <stephanybrazeiro@gmail.com>
2024-11-14 21:47:24 -0300

    feat: add auth with clerk

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,21 @@
-import { Button } from "./_components/ui/button";
+import { UserButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
-const Home = () => {
-  return <Button>Entrar</Button>;
+// import { Button } from "./_components/ui/button";
+
+const Home = async () => {
+  const { userId } = await auth();
+
+  if (!userId) {
+    return redirect("/login");
+  }
+
+  return (
+    <div className="flex h-full items-center justify-center">
+      <UserButton showName />
+    </div>
+  );
 };
 
 export default Home;
